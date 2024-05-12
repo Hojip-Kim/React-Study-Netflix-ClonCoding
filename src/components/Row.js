8388e7416d0c6b5ea282866de6e3c5c8d768c4e4
Hojip-Kim <rlaghwlq12@naver.com>
2024-05-12 22:55:48 +0900

    feat : Row 생성 - Swiper Library 적용, isLargeRow === True?에 따라 LargePoster : SmallPoster로 구분

---
+++ src/components/Row.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import axios from '../api/axios';
+import './Row.css';
+import MovieModal from './MovieModal';
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const Row = (props) => {
+  const { title, id, fetchUrl, isLargeRow } = props;
+
+  const [movies, setMovies] = useState([]);
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const [movieSelected, setmovieSelected] = useState({});
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+  };
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setmovieSelected(movie);
+  };
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+      >
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              ></img>
+            </SwiperSlide>
+          ))}
+        </div>
+      </Swiper>
+
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
+    </section>
+  );
+};
+
+export default Row;
